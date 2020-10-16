15960971dc9d94dbbdb5a9b4e78c03706dff9f7f
claireabely <71153552+claireabely@users.noreply.github.com>
2020-10-15 22:49:47 -0400

    Add files via upload

---
+++ FirstProjectArtAppraisal-master/firstprojectv5/app.js
@@ -0,0 +1,107 @@
+window.onload = function () {
+  
+    var questionArea = document.getElementsByClassName('questions')[0],
+        answerArea   = document.getElementsByClassName('answers')[0],
+        checker      = document.getElementsByClassName('checker')[0],
+        current      = 0,
+    
+        //Ask for help on how to attach the images to these questions, rather than having them hard coded into the index.
+       // An object that holds all the questions + possible answers.
+       // In the array --> last digit gives the right answer position
+        allQuestions = {
+            'What is the correct fair market value of "Jeans" by Albert Oehlen?' : ['1M USD', '600,000 USD' , '120,000 USD', 1],
+
+            'What is the correct fair market value of "Roman Notes V" by Cy Twombly? ' : ['10,000 USD', '50,000 USD', '50,000 USD', 1],
+          
+          'What is the correct fair market value of "Flowers Sent as a Gift" by David Hockney?' : ['7M USD', '2M USD', '5M USD', 0],
+
+          'Fourth Question What is the name of this painting by Grant Wood?' : ['American Dream', 'American Gothic', 'The Farmer and his Wife', 1],
+
+          'Fifth Question Who Became the official painter to Pope Julius II & Leo X?' : ['Raphael', 'Leonardo da Vinci', 'Brunellescchi', 1]
+        };
+        
+    function loadQuestion(curr) {
+    // This function loads all the question into the questionArea
+    // It grabs the current question based on the 'current'-variable
+    
+      var question = Object.keys(allQuestions)[curr];
+      
+      questionArea.innerHTML = '';
+      questionArea.innerHTML = question;    
+    }
+    
+    function loadAnswers(curr) {
+    // This function loads all the possible answers of the given question
+    // It grabs the needed answer-array with the help of the current-variable
+    // Every answer is added with an 'onclick'-function
+    
+      var answers = allQuestions[Object.keys(allQuestions)[curr]];
+      
+      answerArea.innerHTML = '';
+      
+      for (var i = 0; i < answers.length -1; i += 1) {
+        var createDiv = document.createElement('div'),
+            text = document.createTextNode(answers[i]);
+        
+        createDiv.appendChild(text);      
+        createDiv.addEventListener("click", checkAnswer(i, answers));
+        
+        
+        answerArea.appendChild(createDiv);
+      }
+    }
+    
+    function checkAnswer(i, arr) {
+      // This is the function that will run, when clicked on one of the answers
+      // Check if givenAnswer is sams as the correct one
+      // After this, check if it's the last question:
+      // If it is: empty the answerArea and let them know it's done.
+      
+      return function () {
+        var givenAnswer = i,
+            correctAnswer = arr[arr.length-1];
+        
+        if (givenAnswer === correctAnswer) {
+          addChecker(true);             
+        } else {
+          addChecker(false);                        
+        }
+        
+        if (current < Object.keys(allQuestions).length -1) {
+          current += 1;
+          
+          loadQuestion(current);
+          loadAnswers(current);
+        } else {
+          questionArea.innerHTML = 'Done';
+          answerArea.innerHTML = '';
+        }
+                                
+      };
+    }
+    
+    function addChecker(bool) {
+    // This function adds a div element to the page
+    // Used to see if it was correct or false
+    
+      var createDiv = document.createElement('div'),
+          txt       = document.createTextNode(current + 1);
+      
+      createDiv.appendChild(txt);
+      
+      if (bool) {
+        
+        createDiv.className += 'correct';
+        checker.appendChild(createDiv);
+      } else {
+        createDiv.className += 'false';
+        checker.appendChild(createDiv);
+      }
+    }
+    
+    
+    // Start the quiz right away
+    loadQuestion(current);
+    loadAnswers(current);
+    
+  };
