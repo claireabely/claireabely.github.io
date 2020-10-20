dc0baf2afe24fd91e9981a2773b01ede72163bce
claireabely <71153552+claireabely@users.noreply.github.com>
2020-10-19 20:00:13 -0400

    Add files via upload

--- valueproject/highscore.js
+++ valueproject/highscore.js
@@ -7,6 +7,7 @@
 // var highscoresList = document.getElementById("highscoresList");
 // var highscores = JSON.parse(localStorage).getItem("highscores") || [];
 // console.log(highscores);
+// amount of time per question
 
 var initials = document.getElementById("userInitials");
 var submitBtn = document.getElementById("submitBtn");
@@ -44,22 +45,22 @@ $(() => {
 function checkEnd(){
     // if(questions.length === runningQuestion){
         //win
-        const scorePerCent = Math.round(100 * mostRecentScore/numQuestions);
-        
-console.log(scorePerCent);
+        // const scorePerCent = Math.round(100 * mostRecentScore/numQuestions);
+        const score = timeLeft;
+console.log(questions.length);
         
 
-        var img = (scorePerCent >= 80) ? "https://www.sartle.com/media/artwork/laughing-cavalier-frans-hals.jpg" :
-        (scorePerCent >= 60) ? "https://flockler.com/thumbs/sites/889/11_001-e105cd3c-c2c7-499a-aafa-a74df76bde5f_s600x0.jpg" :
-        (scorePerCent >= 40) ? "https://www.sothebys.com/content/dam/stb/lots/N08/N08898/034N08898_6FQFF.jpg.thumb.500.500.png" :
-        (scorePerCent >= 20) ? "https://www.sothebys.com/content/dam/stb/lots/N09/N09497/232N09497_8ZJ76.jpg" :
+        var img = (score >= 240) ? "https://www.sartle.com/media/artwork/laughing-cavalier-frans-hals.jpg" :
+        (score >= 200) ? "https://flockler.com/thumbs/sites/889/11_001-e105cd3c-c2c7-499a-aafa-a74df76bde5f_s600x0.jpg" :
+        (score >= 180) ? "https://www.sothebys.com/content/dam/stb/lots/N08/N08898/034N08898_6FQFF.jpg.thumb.500.500.png" :
+        (score >= 80) ? "https://www.sothebys.com/content/dam/stb/lots/N09/N09497/232N09497_8ZJ76.jpg" :
         "https://www.sothebys.com/content/dam/stb/lots/L19/L19006/101L19006_B89S5.jpg";
 
         var scoreDiv = $("#scoreDiv");
         console.log(scoreDiv);
 scoreDiv.html( "<img src="+ img +">");
 // scoreDiv.append( "<p>"+ scorePerCent +"%</p>");
-finalScore.textContent = scorePerCent+"%";
+finalScore.textContent = score;
 // finalScore.textContent = scorePerCent;
 }
 
